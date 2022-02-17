3490685e8eb7b8fc2e982330ae164baf23bd2793
jappleyard22 <jordan228ra@gmail.com>
2022-02-17 15:17:53 +0000

    Created Items

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,96 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+	private Long ID;
+	private String Name;
+	private Double Price;
+	
+	public Item(Long iD, String name, Double price) {
+		super();
+		ID = iD;
+		Name = name;
+		Price = price;
+	}
+
+	public Item(String name, Double price) {
+		super();
+		Name = name;
+		Price = price;
+		
+	}
+
+	public Long getID() {
+		return ID;
+	}
+
+	public void setID(Long iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public Double getPrice() {
+		return Price;
+	}
+
+	public void setPrice(Double price) {
+		Price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [ID=" + ID + ", Name=" + Name + ", Price=" + Price + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		result = prime * result + ((Name == null) ? 0 : Name.hashCode());
+		result = prime * result + ((Price == null) ? 0 : Price.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (ID == null) {
+			if (other.ID != null)
+				return false;
+		} else if (!ID.equals(other.ID))
+			return false;
+		if (Name == null) {
+			if (other.Name != null)
+				return false;
+		} else if (!Name.equals(other.Name))
+			return false;
+		if (Price == null) {
+			if (other.Price != null)
+				return false;
+		} else if (!Price.equals(other.Price))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
+
