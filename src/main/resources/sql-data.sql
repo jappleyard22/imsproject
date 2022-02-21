108567df31b7bb6e4f3bcdfde2db90366d484c5b
jordan22 <jordan228ra@gmail.com>
2022-02-21 01:17:13 +0000

    added order classes and SQL

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,3 +1,7 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
-INSERT INTO `items` (`item_name`, `price`) VALUES ('bowl', 5);
+INSERT INTO `items` (`item_name`, `price`) VALUES ('bowl', 10);
 INSERT INTO `items` (`item_name`, `price`) VALUES ('dish', 5);
+INSERT INTO `orders` (`fk_cust_id`, `price`) VALUES (1, 15);
+INSERT INTO `order_items` (`fk_order_id`, `fk_item_id`) VALUES (1, 1);
+INSERT INTO `order_items` (`fk_order_id`, `fk_item_id`) VALUES (1, 2);
+
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -17,6 +17,25 @@ CREATE TABLE IF NOT EXISTS`imsproject`.`items`(
     `price` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`item_id`)
 ); 
+
+CREATE TABLE IF NOT EXISTS`imsproject`.`orders`(
+	`order_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `fk_cust_id` INT(11) NOT NULL,
+    PRIMARY KEY (`order_id`),
+    FOREIGN KEY (`fk_cust_id`)
+		REFERENCES `imsproject`.`customers` (`cust_id`) ON DELETE CASCADE,
+        `price` DOUBLE
+	);
+    
+CREATE TABLE IF NOT EXISTS `imsproject`.`order_items` (
+	`fk_order_id` INT NOT NULL,
+    `fk_item_id` INT NOT NULL,
+    CONSTRAINT `fk_order_items_1` FOREIGN KEY (`fk_order_id`)
+		REFERENCES `imsproject`.`orders` (`order_id`) ON DELETE CASCADE,
+	CONSTRAINT `order_items_2` FOREIGN KEY (`fk_item_id`)
+		REFERENCES `imsproject`.`items` (`item_id`) ON DELETE CASCADE
+	);
+    
     
 
     
